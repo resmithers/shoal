886019c40625c7f695722902fc87b37231fb8bf5
resmithers <rory.smith123@hotmail.co.uk>
2019-03-22 12:17:26 +0000

    Amended ListenVotes-unique by author (most recent)

--- src/utils/FirestoreListen.js
+++ src/utils/FirestoreListen.js
@@ -1,55 +1,62 @@
-const {db} = require('./config.js');
-import moment from 'moment';
+const { db } = require("./config.js");
+import moment from "moment";
 
 export function listenAllDiscussions(thisBind) {
-  return db
-    .collection('Discussions')
-    .onSnapshot((snap) => {
-       thisBind.discussions = snap.docs.map((v) =>{ return { id:v.id, ...v.data()} } );
+  return db.collection("Discussions").onSnapshot(snap => {
+    thisBind.discussions = snap.docs.map(v => {
+      return { id: v.id, ...v.data() };
     });
+  });
 }
 
 export function listenDisc(thisBind) {
   return db
-    .collection('Discussions')
+    .collection("Discussions")
     .doc(thisBind.discID)
-    .collection('Points')
-    .onSnapshot((snap) => {
-      thisBind.keyPoints = snap.docs.map((v) => v.data());
+    .collection("Points")
+    .onSnapshot(snap => {
+      thisBind.keyPoints = snap.docs.map(v => v.data());
     });
 }
 
 export function listenComments(thisBind) {
   return db
-    .collection('Discussions')
+    .collection("Discussions")
     .doc(thisBind.discID)
-    .collection('Comments')
-    .orderBy('Timestamp', 'desc')
-    .onSnapshot((snap) => {
-      thisBind.comments = snap.docs.map((v) => v.data());
+    .collection("Comments")
+    .orderBy("Timestamp", "desc")
+    .onSnapshot(snap => {
+      thisBind.comments = snap.docs.map(v => v.data());
     });
 }
 
 export function listenVotes(thisBind) {
   return db
-    .collection('Discussions')
+    .collection("Discussions")
     .doc(thisBind.discID)
-    .collection('Votes')
-    .orderBy('Timestamp', 'asc')
-    .onSnapshot((snap) => {
+    .collection("Votes")
+    .orderBy("Timestamp", "asc")
+    .onSnapshot(({ docs }) => {
+      const data = docs.map(v => v.data());
       const accumulatedVotes = [];
-      const getVoteData = (votes) => {
-        votes.reduce((acc, num) => {
-          acc += num;
-          accumulatedVotes.push(acc);
-          return acc;
-        }, 0);
-      };
-      const converted = snap.docs.map((v) => v.data().Vote);
-      getVoteData(converted);
+      const labels = [];
+
+      getUnique(data, "Author").reduce((acc, vObj) => {
+        acc += vObj.Vote;
+        labels.push(moment(vObj.Timestamp).format("LLL"));
+        accumulatedVotes.push(acc);
+        return acc;
+      }, 0);
+
       thisBind.datasets = accumulatedVotes;
-      thisBind.labels = snap.docs.map((v) =>
-        moment(v.data().Timestamp).format('LLL')
-      );
+      thisBind.labels = labels;
     });
+}
+
+function getUnique(arr, comp) {
+  return arr
+    .map(e => e[comp])
+    .map((e, i, final) => final.lastIndexOf(e) === i && i)
+    .filter(e => arr[e])
+    .map(e => arr[e]);
 }
