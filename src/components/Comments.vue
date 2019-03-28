14426213d695d5d8cb7479bda8ced8e4035032f5
resmithers <rory.smith123@hotmail.co.uk>
2019-03-28 18:01:15 +0000

    Stuff

--- src/components/Comments.vue
+++ src/components/Comments.vue
@@ -1,18 +1,26 @@
 <template>
-<div class="d-flex justify-content-center">
-  <div class="d-flex-column Comments w-100">
-    <h1 class="text-center mt-5">Comments:</h1>
-    <div class="coms">
-    <b-card v-for="comment in comments" :key="comment.id" class="w-100 text-center" style="min-height: 110px; ">
+  <div class="d-flex justify-content-center">
+    <div class="d-flex-column Comments w-100">
+      <h1 class="text-center mt-5">Comments:</h1>
+      <div class="coms">
+        <b-card
+          v-for="comment in comments"
+          :key="comment.id"
+          class="w-100 text-center"
+          style="min-height: 110px; "
+        >
           <b-card-body>
-           <span> {{ comment.Author }} : </span><span class="body"> {{ comment.Body }} </span>
+            <span>{{ comment.Author }} :</span>
+            <span class="body">{{ comment.Body }}</span>
             <br>
-           <span class="timestamp"> {{ moment(comment.Timestamp).format("LLL") }} </span>
-             
+            <span class="timestamp">{{ moment(comment.Timestamp).format("LLL") }}</span>
           </b-card-body>
-        </b-card>  
-        </div>
-</div>
+          <b-modal>
+            <b-modal-title>Hi</b-modal-title>
+          </b-modal>
+        </b-card>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -57,12 +65,10 @@ span {
   color: rgb(6, 6, 112);
 }
 
-
 .body {
   color: white;
   font-weight: 200;
   font-size: 26px;
-
 }
 
 .timestamp {
@@ -73,7 +79,7 @@ span {
 
 .coms {
   min-height: 110px;
-  max-height:80vh;
+  max-height: 80vh;
   overflow-y: scroll;
   overflow-x: hidden;
   margin-bottom: 20px;
@@ -81,6 +87,6 @@ span {
 }
 
 .coms::-webkit-scrollbar {
- width: 0 !important;
+  width: 0 !important;
 }
 </style>
